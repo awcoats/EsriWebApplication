7da354578d4d56ac2ddc1e356233f8c221fa029f
Andrew Coats <acoats@calicosol.com>
2016-03-01 12:29:12 -0700

    Scalebar and beginning of a toolbox.

--- EsiWebApplication/CalAgMap/MapController.ts
+++ EsiWebApplication/CalAgMap/MapController.ts
@@ -3,7 +3,20 @@
 import esri = require("esri");
 import Map = require("esri/map");
 import Point = require("esri/geometry/Point");
-
+import Scalebar = require("esri/dijit/Scalebar");
+//import BasemapGallery = require("esri/dijit/BasemapGallery");
+import Polygon = require("esri/geometry/Polygon");
+import Draw = require("esri/toolbars/draw")
+import Edit = require("esri/toolbars/edit");
+import SimpleMarkerSymbol = require("esri/symbols/SimpleMarkerSymbol");
+import SimpleLineSymbol = require("esri/symbols/SimpleLineSymbol");
+import SimpleFillSymbol =  require("esri/symbols/SimpleFillSymbol"),
+import Graphic = require("esri/graphic");
+import jsonUtils = require("esri/geometry/jsonUtils");
+import Color = require("esri/Color");
+//import Menu = require("esri/dijit/Menu");
+//import MenuItem = require("dijit/MenuItem");
+//import MenuSeperator = require("dijit/MenuSeparator");
 export = MapController;
 
 class MapController {
@@ -21,5 +34,21 @@ class MapController {
         mapOptions.zoom = 12;
 
         this.map = new Map(this.mapDiv, mapOptions);
+
+        this.addScaleBar();
+        
+      
     }
+
+    private addScaleBar() {
+        var scalebar = new Scalebar({
+            map: this.map,
+            // "dual" displays both miles and kilmometers
+            // "english" is the default, which displays miles
+            // use "metric" for kilometers
+            scalebarUnit: "dual"
+        });
+    }
+
+   
 }
